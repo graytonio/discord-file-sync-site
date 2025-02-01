dacb24ae5bcf9086274da3506681c8345210bbe7
Grayton Ward <graytonio.ward@gmail.com>
2025-01-31 21:07:18 -0500

    feat: features section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,61 @@ import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await auth();
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  Cog6ToothIcon,
+  FingerPrintIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from '@heroicons/react/20/solid'
 
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
+const features = [
+
+
+  {
+    name: 'Push to deploy.',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'SSL certificates.',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Simple queues.',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus voluptas blanditiis et.',
+    icon: ArrowPathIcon,
+  },
+  {
+    name: 'Advanced security.',
+    description: 'Iure sed ab. Aperiam optio placeat dolor facere. Officiis pariatur eveniet atque et dolor.',
+    icon: FingerPrintIcon,
+  },
+  {
+    name: 'Powerful API.',
+    description: 'Laudantium tempora sint ut consectetur ratione. Ut illum ut rem numquam fuga delectus.',
+    icon: Cog6ToothIcon,
+  },
+  {
+    name: 'Database backups.',
+    description: 'Culpa dolorem voluptatem velit autem rerum qui et corrupti. Quibusdam quo placeat.',
+    icon: ServerIcon,
+  },
+]
+
+export default async function Home() {
+  // const hello = await api.post.hello({ text: "from tRPC" });
+  // const session = await auth();
+
+  // if (session?.user) {
+  //   void api.post.getLatest.prefetch();
+  // }
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="min-h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         {/* <div className="bg-white"> */}
           <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
             <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
@@ -58,7 +102,7 @@ export default async function Home() {
                 </div>
               </div>
               <div className="relative mt-16 h-80 lg:mt-8">
-                <img
+                <img // TODO Replace image
                   alt="App screenshot"
                   src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png"
                   width={1824}
@@ -69,6 +113,31 @@ export default async function Home() {
             </div>
           {/* </div> */}
         </div>
+        <div className="bg-gray-900 py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-base/7 font-semibold text-indigo-400">Everything you need</h2>
+          <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl">
+            No server? No problem.
+          </p>
+          <p className="mt-6 text-lg/8 text-gray-300">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste
+            dolor cupiditate blanditiis.
+          </p>
+        </div>
+        <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base/7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-16">
+          {features.map((feature) => (
+            <div key={feature.name} className="relative pl-9">
+              <dt className="inline font-semibold text-white">
+                <feature.icon aria-hidden="true" className="absolute left-1 top-1 size-5 text-indigo-500" />
+                {feature.name}
+              </dt>{' '}
+              <dd className="inline">{feature.description}</dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
       </main>
     </HydrateClient>
   );
