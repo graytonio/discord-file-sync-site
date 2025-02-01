bb8d262efaec8d9cec67478534646f93012f03a3
Grayton Ward <graytonio.ward@gmail.com>
2025-01-31 21:08:55 -0500

    chore: formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,42 +11,46 @@ import {
   FingerPrintIcon,
   LockClosedIcon,
   ServerIcon,
-} from '@heroicons/react/20/solid'
+} from "@heroicons/react/20/solid";
 
 const features = [
-
-
   {
-    name: 'Push to deploy.',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
+    name: "Push to deploy.",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.",
     icon: CloudArrowUpIcon,
   },
   {
-    name: 'SSL certificates.',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    name: "SSL certificates.",
+    description:
+      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
     icon: LockClosedIcon,
   },
   {
-    name: 'Simple queues.',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus voluptas blanditiis et.',
+    name: "Simple queues.",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus voluptas blanditiis et.",
     icon: ArrowPathIcon,
   },
   {
-    name: 'Advanced security.',
-    description: 'Iure sed ab. Aperiam optio placeat dolor facere. Officiis pariatur eveniet atque et dolor.',
+    name: "Advanced security.",
+    description:
+      "Iure sed ab. Aperiam optio placeat dolor facere. Officiis pariatur eveniet atque et dolor.",
     icon: FingerPrintIcon,
   },
   {
-    name: 'Powerful API.',
-    description: 'Laudantium tempora sint ut consectetur ratione. Ut illum ut rem numquam fuga delectus.',
+    name: "Powerful API.",
+    description:
+      "Laudantium tempora sint ut consectetur ratione. Ut illum ut rem numquam fuga delectus.",
     icon: Cog6ToothIcon,
   },
   {
-    name: 'Database backups.',
-    description: 'Culpa dolorem voluptatem velit autem rerum qui et corrupti. Quibusdam quo placeat.',
+    name: "Database backups.",
+    description:
+      "Culpa dolorem voluptatem velit autem rerum qui et corrupti. Quibusdam quo placeat.",
     icon: ServerIcon,
   },
-]
+];
 
 export default async function Home() {
   // const hello = await api.post.hello({ text: "from tRPC" });
@@ -59,85 +63,90 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main className="min-h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        {/* <div className="bg-white"> */}
-          <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
-            <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-              <svg
-                viewBox="0 0 1024 1024"
-                aria-hidden="true"
-                className="absolute left-1/2 top-1/2 -z-10 size-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
-              >
-                <circle
-                  r={512}
-                  cx={512}
-                  cy={512}
-                  fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
-                  fillOpacity="0.7"
-                />
-                <defs>
-                  <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                    <stop stopColor="#7775D6" />
-                    <stop offset={1} stopColor="#E935C1" />
-                  </radialGradient>
-                </defs>
-              </svg>
-              <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-                <h2 className="text-balance text-3xl font-semibold tracking-tight text-white sm:text-4xl">
-                  Collaborate Better. Stay In Sync
-                </h2>
-                <p className="mt-6 text-pretty text-lg/8 text-gray-300">
-                  Manage your important announcements and messages outside of Discord.
-                  Keep your community up to date without needing to fumble with editing.
-                </p>
-                <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-                  <a
-                    href="#"
-                    className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                  >
-                    Get started
-                  </a>
-                  <a href="#" className="text-sm/6 font-semibold text-white">
-                    Learn more <span aria-hidden="true">→</span>
-                  </a>
-                </div>
-              </div>
-              <div className="relative mt-16 h-80 lg:mt-8">
-                <img // TODO Replace image
-                  alt="App screenshot"
-                  src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png"
-                  width={1824}
-                  height={1080}
-                  className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
-                />
+        <div className="mx-auto max-w-7xl py-12 sm:px-6 sm:py-32 lg:px-8">
+          <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+            <svg
+              viewBox="0 0 1024 1024"
+              aria-hidden="true"
+              className="absolute left-1/2 top-1/2 -z-10 size-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+            >
+              <circle
+                r={512}
+                cx={512}
+                cy={512}
+                fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+                fillOpacity="0.7"
+              />
+              <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                  <stop stopColor="#7775D6" />
+                  <stop offset={1} stopColor="#E935C1" />
+                </radialGradient>
+              </defs>
+            </svg>
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+              <h2 className="text-balance text-3xl font-semibold tracking-tight text-white sm:text-4xl">
+                Collaborate Better. Stay In Sync
+              </h2>
+              <p className="mt-6 text-pretty text-lg/8 text-gray-300">
+                Manage your important announcements and messages outside of
+                Discord. Keep your community up to date without needing to
+                fumble with editing.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                <a
+                  href="#"
+                  className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                >
+                  Get started
+                </a>
+                <a href="#" className="text-sm/6 font-semibold text-white">
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
               </div>
             </div>
-          {/* </div> */}
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <img // TODO Replace image
+                alt="App screenshot"
+                src="https://tailwindui.com/plus/img/component-images/dark-project-app-screenshot.png"
+                width={1824}
+                height={1080}
+                className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+              />
+            </div>
+          </div>
         </div>
         <div className="bg-gray-900 py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-base/7 font-semibold text-indigo-400">Everything you need</h2>
-          <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl">
-            No server? No problem.
-          </p>
-          <p className="mt-6 text-lg/8 text-gray-300">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste
-            dolor cupiditate blanditiis.
-          </p>
-        </div>
-        <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base/7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-16">
-          {features.map((feature) => (
-            <div key={feature.name} className="relative pl-9">
-              <dt className="inline font-semibold text-white">
-                <feature.icon aria-hidden="true" className="absolute left-1 top-1 size-5 text-indigo-500" />
-                {feature.name}
-              </dt>{' '}
-              <dd className="inline">{feature.description}</dd>
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:mx-0">
+              <h2 className="text-base/7 font-semibold text-indigo-400">
+                Everything you need
+              </h2>
+              <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-white sm:text-5xl">
+                No server? No problem.
+              </p>
+              <p className="mt-6 text-lg/8 text-gray-300">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores impedit perferendis suscipit eaque, iste dolor
+                cupiditate blanditiis.
+              </p>
             </div>
-          ))}
-        </dl>
-      </div>
-    </div>
+            <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base/7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-16">
+              {features.map((feature) => (
+                <div key={feature.name} className="relative pl-9">
+                  <dt className="inline font-semibold text-white">
+                    <feature.icon
+                      aria-hidden="true"
+                      className="absolute left-1 top-1 size-5 text-indigo-500"
+                    />
+                    {feature.name}
+                  </dt>{" "}
+                  <dd className="inline">{feature.description}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
       </main>
     </HydrateClient>
   );
